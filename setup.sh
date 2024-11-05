66f0171a36b6b9c1bc44df491471980a464375aa
helloshowup <hello@showup.courses>
2024-11-05 11:36:34 +0200

    Add SQLite installation requirements

---
+++ setup.sh
@@ -0,0 +1,17 @@
+﻿#!/bin/bash
+
+# Download and install newer SQLite
+wget https://www.sqlite.org/2024/sqlite-autoconf-3450100.tar.gz
+tar xzf sqlite-autoconf-3450100.tar.gz
+cd sqlite-autoconf-3450100
+./configure
+make
+make install
+
+# Update library path
+echo "/usr/local/lib" > /etc/ld.so.conf.d/sqlite3.conf
+ldconfig
+
+# Clean up
+cd ..
+rm -rf sqlite-autoconf-3450100*
